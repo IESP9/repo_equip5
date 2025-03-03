95fac23f7af6cc48d376d8d273d52fc9694ab317
raizar22 <aiman.messaudi@gmail.com>
2025-03-03 12:26:04 +0100

    Primera fase prototipo (acabado)

--- extrem-combat/scripts/personaje_1(preuba).gd
+++ extrem-combat/scripts/personaje_1(preuba).gd
@@ -20,6 +20,7 @@ func _physics_process(delta: float) -> void:
 	if direction:
 		velocity.x = direction * SPEED
 	else:
+		
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
---
+++ extrem-combat/walk.gd
@@ -0,0 +1,25 @@
+extends PlayerState
+
+
+
+func enter(previous_state_path: String, data := {}) -> void:
+	sprite.play("walk")
+	state_label.text = "WALK"
+
+func physics_update(delta: float) -> void:
+	var input_direction_x := Input.get_axis("Left", "Right")
+	player.velocity.x = player.speed * input_direction_x
+
+	if input_direction_x < 0:
+		sprite.flip_h = true
+	if input_direction_x > 0:
+		sprite.flip_h = false
+	player.move_and_slide()
+
+
+	#if not player.is_on_floor():
+		#finished.emit(FALLING)
+	if Input.is_action_just_pressed("Spacebar"):
+		finished.emit(JUMPING)
+	elif is_equal_approx(input_direction_x, 0.0):
+		finished.emit(IDLE)
