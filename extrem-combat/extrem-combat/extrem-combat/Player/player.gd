28bfc641829731cd8683537e18608ef00ad16eb5
raizar22 <26631812c@iespoblenou.org>
2025-03-03 12:32:04 +0100

    Primera fase prototipo
    El anterior esta subido con un perfil equivocado

---
+++ extrem-combat/extrem-combat/extrem-combat/Player/player.gd
@@ -0,0 +1,6 @@
+class_name Player
+extends CharacterBody2D
+
+
+var speed = 250.0
+var jump_velocity = -350.0
---
+++ extrem-combat/extrem-combat/extrem-combat/jumping.gd
@@ -0,0 +1,44 @@
+extends PlayerState
+
+# Variables para controlar el salto
+var jump_velocity := -400.0  # Velocidad inicial del salto 
+var gravity := 800.0  # Gravedad 
+var velocity_y := 1.0  # Velocidad vertical
+
+func enter(previous_state_path: String, data := {}) -> void:
+	# Inicio del salto
+	velocity_y = jump_velocity
+	sprite.play("jumping")  
+	state_label.text = "JUMPING"
+
+func physics_update(delta: float) -> void:
+	# Aplicar gravedad
+	velocity_y += gravity * delta 
+
+	# Mover al jugador verticalmente
+	player.velocity.y = velocity_y
+
+	# Mover al jugador horizontalmente 
+	var input_direction_x := Input.get_axis("Left", "Right")
+	player.velocity.x = player.speed * input_direction_x
+
+	# Girar sprite
+	if input_direction_x < 0:
+		sprite.flip_h = true
+	elif input_direction_x > 0:
+		sprite.flip_h = false
+
+	# Mover al jugador
+	player.move_and_slide()
+
+	# Cambiar a estado de caída si la velocidad vertical es positiva
+	if velocity_y > 0:
+		finished.emit(FALLING)
+
+	# Cambiar a estado de caminar si el jugador toca el suelo
+	if player.is_on_floor():
+		finished.emit(IDLE)
+
+	# Doble salto
+	if Input.is_action_just_pressed("Spacebar"):
+		finished.emit(JUMPING)
