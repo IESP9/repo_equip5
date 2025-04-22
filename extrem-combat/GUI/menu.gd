d1ad1cdd26af5aeaad01ae57c5c261efa6683ec1
AimanBM <26631812c@iespoblenou.org>
2025-04-23 01:59:13 +0200

    opcion de controles
    controles añadidos

--- extrem-combat/GUI/menu.gd
+++ extrem-combat/GUI/menu.gd
@@ -6,7 +6,7 @@ func _on_play_pressed() -> void:
 
 
 func _on_controls_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://controles.tscn")
 
 
 func _on_quit_pressed() -> void:
